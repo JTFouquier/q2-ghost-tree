180b92049468c18e033e728c456089cbb71c38cc
Jennifer Fouquier <jennietf@gmail.com>
2016-12-20 15:31:21 -0700

    add cluster command; start OtuMap semantic type

---
+++ q2_ghost_tree/_extensions_cluster.py
@@ -0,0 +1,45 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2015--, ghost-tree development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the LICENSE file, distributed with this software.
+# ----------------------------------------------------------------------------
+import os
+
+# (TODO)
+def extensions_cluster(extensions_sequences_fp: str,
+                       similarity_threshold: float,
+                       otu_formatted_fp: str):
+
+    def preprocess_extension_tree_sequences(extension_sequences_fp,
+                                            similarity_threshold,
+                                            otu_formatted_fp):
+        """Creates an OTU cluster formatted file from a .fasta sequence file.
+        Creates an OTU cluster formatted file from a sequence file in .fasta
+        format using Sumaclust (http://metabarcoding.org/sumatra). Sequence file
+        can be from a database or from representative sequences specific to one's
+        own dataset. OTU clusters will be created according to the threshold.
+        The lower the threshold, the more "unidentified" sequences will be
+        accounted for, but this will create larger "extension" groups that form
+        less accurate multiple sequence alignments. Changing the threshold value
+        to a number drastically different from the standard 97 or 99 OTU similarity
+        will alter the resulting tree improving or reducing its utility in
+        diversity analyses and should be performed with caution.
+        Parameters
+        __________
+        extension_sequences_fp : filepath
+            Must be a .fasta formatted file from a database or representative
+            sequences from a dataset. Usually these are 97 or 99 percent OTU
+            sequences.
+        similarity_threshold : float
+            Value ranging from 0.00 to 1.00. Changing the threshold value to a
+            number drastically different from the standard 97 or 99 OTU similarity
+            will alter the resulting tree improving or reducing its utility in
+            diversity analyses and should be performed with caution.
+        otu_formatted_fp : filepath
+            Name of resulting OTU cluster formatted filehandle.
+        """
+        similarity_threshold = str(similarity_threshold)
+        os.system("sumaclust -g -f -t "+similarity_threshold+" -O " +
+                  "" + otu_formatted_fp + " " + extension_sequences_fp + "")
--- q2_ghost_tree/plugin_setup.py
+++ q2_ghost_tree/plugin_setup.py
@@ -1,16 +1,18 @@
 import qiime.plugin
-from qiime.plugin import (Properties)
-
 import q2_ghost_tree
 
-# These imports are only included to support the example methods and
-# visualizers. Remove these imports when you are ready to develop your plugin.
-from q2_dummy_types import IntSequence1, IntSequence2, Mapping
-from q2_types.feature_table import ( FeatureTable, Frequency, RelativeFrequency, PresenceAbsence)
-from q2_types.feature_data import (FeatureData, Sequence, Taxonomy, AlignedSequence)
-from q2_types.tree import (Phylogeny, Rooted)
+from q2_dummy_types import IntSequence1, IntSequence2
+from q2_types.feature_data import FeatureData, Sequence, Taxonomy, AlignedSequence
+from q2_types.tree import Phylogeny, Rooted
+
+
 from ._dummy_method import concatenate_ints
-from ._hybrid_tree import scaffold_hybrid_tree
+from ._scaffold_hybrid_tree import scaffold_hybrid_tree
+
+
+import qiime.plugin.model as model
+
+
 # (TODO) need to register all code here
 
 plugin = qiime.plugin.Plugin(
@@ -28,9 +30,92 @@ plugin = qiime.plugin.Plugin(
     citation_text=None
 )
 
+"""
+MAKE SEMANTIC TYPES HERE? Not working
+"""
+
+# Define semantic types.
+OtuMap = qiime.plugin.SemanticType('OtuMap')
+
+# Register semantic types on the plugin.
+plugin.register_semantic_types('OtuMap')
+
+###############################################################################
+#
+# OtuMap
+#
+#     Groups of OTUs clustered by a similarity threshold. Each line contains
+#     tab separated OTUs that are within the similarity threshold.
+#
+###############################################################################
+
+# Define a file format for use in the directory format defined below.
+class OtuMapFormat(model.TextFileFormat):
+    # Sniffers are used when reading and writing data from a file. A sniffer
+    # determines whether a file appears to conform to its file format. Sniffers
+    # should only read a small piece of the file to determine membership and
+    # should not rely on file extension.
+    def sniff(self):
+        with self.open() as fh:
+            for line, _ in zip(fh, range(5)):
+                try:
+                    print ('TESTING INSIDE OTU MAP SNIFFER')
+                    int(line.rstrip('\n'))
+                except (TypeError, ValueError):
+                    return False
+            return True
+
+# Define a directory format. A directory format is a directory structure
+# composed of one or more files (nested directories are also supported). Each
+# file has a specific file format associated with it.  This directory format
+# only has a single file, ints.txt, with file format `IntSequenceFormat`.
+OtuMapDirectoryFormat = model.SingleFileDirectoryFormat('OtuMapDirectoryFormat', 'ints.txt', OtuMapFormat)
+
+# Register the formats defined above. Formats must be unique across all
+# plugins installed on a users system.
+plugin.register_formats(OtuMapFormat, OtuMapDirectoryFormat)
+
+# Register the directory format with the semantic types defined above. A
+# directory format can be registered to multiple semantic types. Currently, a
+# semantic type can only have a single directory format associated with it.
+plugin.register_semantic_type_to_format(OtuMap, artifact_format=OtuMapDirectoryFormat)
+
+# (TODO): FIX function and definition
+# Define a transformer for converting a file format (`IntSequenceFormat`) into
+# a view type (`list` in this case). To indicate that only the QIIME 2
+# Framework should interact with a transformer, a non-meaningful name is used.
+# The convention is `_<int counter>`, but anything is acceptable. The aim is to
+# draw the reader to the function annotations, which convey precisely what the
+# transformer is responsible for.
+@plugin.register_transformer
+def _1(ff: OtuMapFormat) -> list:
+    with ff.open() as fh:
+        data = [] # becomes a list of lists (otus in one similarity threshold)
+        for line in fh:
+            otus = line.split('\t')
+            data.append(otus)
+        return data
+
+# (TODO) FIX function and definition... otus groups are in lines/lists
+# Define a transformer for converting a view type (`list` in this case) to the
+# file format (`IntSequenceFormat`). To indicate that only the QIIME 2
+# Framework should interact with a transformer, a non-meaningful name is used.
+# The convention is `_<int counter>`, but anything is acceptable. The aim is to
+# draw the reader to the function annotations, which convey precisely what the
+# transformer is responsible for.
+@plugin.register_transformer
+def _2(data: list) -> OtuMapFormat:
+    otu_map = OtuMapFormat()
+    with otu_map.open() as fh:
+        for otu_list in data:
+            fh.write('%d\n' % '\t'.join(otu_list))
+    return otu_map
+
+
 # The next two code blocks are examples of how to register methods and
 # visualizers. Replace them with your own registrations when you are ready to
 # develop your plugin.
+
 
 plugin.methods.register_function(
     function=concatenate_ints,
@@ -54,7 +139,7 @@ plugin.methods.register_function(
 plugin.methods.register_function(
     function=scaffold_hybrid_tree,
     inputs={
-        'otus_fh': FeatureTable[PresenceAbsence],
+        'otus_fh': OtuMap, # ghost-tree semantic type
         'extension_taxonomy_fh': FeatureData[Taxonomy],
         'extension_seq_fh': FeatureData[Sequence],
         'foundation_alignment_fh': FeatureData[AlignedSequence]
@@ -66,4 +151,18 @@ plugin.methods.register_function(
     ],
     name='scaffold-hybrid-tree',
     description='This method creates a hybrid-gene phylogenetic tree.'
+)
+
+plugin.methods.register_function(
+    function=extensions_cluster,
+    inputs={
+        'extensions_sequences_fp': FeatureData[Sequence],
+    },
+    parameters={ 'similarity_threshold': float
+    },
+    outputs=[
+        ('otu_formatted_fp', OtuMap),
+    ],
+    name='extensions_cluster',
+    description='Groups sequences in .fasta file by similarity threshold'
 )
