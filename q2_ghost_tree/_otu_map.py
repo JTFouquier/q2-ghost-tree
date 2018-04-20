0b3905be4f26229bfb3d71778c9dd57d3381db52
Jennifer Fouquier <jennietf@gmail.com>
2018-04-19 21:11:50 -0600

    Use q2 taxonomy; gt OtuMap; ghost-tree works
    ghost-tree works for foundation alignement and not tree; also works for different graft levels.

--- q2_ghost_tree/_otu_map.py
+++ q2_ghost_tree/_otu_map.py
@@ -22,29 +22,32 @@ import qiime2.plugin.model as model
 class OtuMapFormat(model.TextFileFormat):
 
     def sniff(self):
-        with self.open() as fh:
+        # TODO this needs to check the file
+        return True
 
-            for line, _ in zip(fh, range(5)):
-
-                # (NOTE) unsure how to validate an OTU file!
-
-                # check that it's a string
-                try:
-                    if type(line) == str:
-                        pass
-                    else:
-                        return False
-                except:
-                    return False
-
-                # shouldn't contain special chars
-                try:
-                    if not re.search(';', line):
-                        pass
-                except:
-                    pass
-
-            return True
+        # with self.open() as fh:
+        #
+        #     for line, _ in zip(fh, range(5)):
+        #
+        #         # (NOTE) unsure how to validate an OTU file!
+        #
+        #         # check that it's a string
+        #         try:
+        #             if type(line) == str:
+        #                 pass
+        #             else:
+        #                 return False
+        #         except:
+        #             return False
+        #
+        #         # shouldn't contain special chars
+        #         try:
+        #             if not re.search(';', line):
+        #                 pass
+        #         except:
+        #             pass
+        #
+        #     return True
 
 OtuMapDirectoryFormat = model.SingleFileDirectoryFormat(
     'OtuMapDirectoryFormat', 'otus.txt', OtuMapFormat)
