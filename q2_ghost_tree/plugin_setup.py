2bdd2d52081233de739ddb4e593fd629ddf2be25
Jennifer Fouquier <jennietf@gmail.com>
2018-05-11 08:56:33 -0700

    graft level param requirements (q2.plugin); help docs

--- q2_ghost_tree/plugin_setup.py
+++ q2_ghost_tree/plugin_setup.py
@@ -7,7 +7,8 @@ from q2_types.tree import Phylogeny, Rooted, Unrooted
 import q2_ghost_tree
 
 # import plugin 'wrapper' code to original ghost-tree code
-from ._scaffold_hybrid_tree import scaffold_hybrid_tree_foundation_alignment
+from ._scaffold_hybrid_tree_foundation_alignment import \
+    scaffold_hybrid_tree_foundation_alignment
 from ._scaffold_hybrid_tree_foundation_tree import \
     scaffold_hybrid_tree_foundation_tree
 from ._extensions_cluster import extensions_cluster
@@ -20,6 +21,7 @@ from ._silva_taxonomy import SilvaTaxonomyFormat, SilvaTaxonomyDirectoryFormat
 from ._silva_accession import SilvaAccessionFormat, \
     SilvaAccessionDirectoryFormat
 
+# TODO Citations are not listed correctly here
 # initiate Qiime2 plugin
 plugin = qiime2.plugin.Plugin(
     name='ghost-tree',
@@ -28,7 +30,12 @@ plugin = qiime2.plugin.Plugin(
                 'into one phylogenetic tree that can be used for diversity '
                 'analyses. One database is used as a "foundation tree" '
                 'because it provides better phylogeny across all phyla, '
-                'and the other database provides finer taxonomic resolution.',
+                'and the other database provides finer taxonomic resolution.\n'
+                ''
+                '\nPlease cite: ghost-tree: creating hybrid-gene phylogenetic '
+                'trees for diversity analyses. Fouquier J, Rideout JR, '
+                'Bolyen E, Chase J, Shiffer A, McDonald D, Knight R, Caporaso '
+                'JG, and Kelley ST',
     version=q2_ghost_tree.__version__,
     website='https://github.com/JTFouquier/ghost-tree',
     package='q2_ghost_tree',
@@ -49,6 +56,11 @@ plugin.register_semantic_type_to_format(OtuMap,
 # Register all methods used by ghost-tree
 # TODO add descriptions here... what is the point of name... redundant on help
 # TODO pages
+
+graft_level = qiime2.plugin
+graft_level = graft_level.Str % \
+              graft_level.Choices({'p', 'c', 'o', 'f', 'g'})
+
 plugin.methods.register_function(
     function=scaffold_hybrid_tree_foundation_alignment,
     inputs={
@@ -58,11 +70,37 @@ plugin.methods.register_function(
         'foundation_alignment': FeatureData[AlignedSequence],
             },
     parameters={
-        'graft_level': qiime2.plugin.Str,
+        'graft_level': graft_level,
                 },
     outputs=[
         ('ghost_tree', Phylogeny[Rooted]),
     ],
+    input_descriptions={
+        'otu_map': 'OtuMap. The output from the command "extensions-cluster". '
+                   'Each line in an OtuMap contains sequences that are a '
+                   'certain percent similar to one another.',
+        'extension_taxonomy': 'FeatureData[Taxonomy] The taxonomy for the '
+                              'extension sequences for a ghost tree.',
+        'extension_sequences': 'FeatureData[Sequence] The extension sequences '
+                               'for the ghost tree. The extensions are '
+                               'grafted onto a foundation tree.',
+        'foundation_alignment': 'FeatureData[AlignedSequence] Aligned '
+                                'sequences that will be used as the '
+                                'foundation tree for a ghost tree.',
+    },
+    parameter_descriptions={
+        'graft_level': 'The taxonomic rank you would like to group extension '
+                       'sequences by prior to having them grafted onto the '
+                       'foundation. Rank choices are p: phylum, c: class, '
+                       'o: order, f: family, g: genus. Please note grafting '
+                       'at higher taxonomic ranks than genus is experimental, '
+                       'but may be useful for marker-gene regions which have '
+                       'less optimal taxonomic resolution.',
+    },
+    output_descriptions={
+        'ghost_tree': 'Phylogeny[Rooted]. This is your final ghost tree output'
+                      'as a .qza. Extracted it will be in Newick format.'
+    },
     name='scaffold-hybrid-tree-foundation-alignment',
     description='Create a hybrid-gene phylogenetic tree using an '
                 'alignment as a foundation.'
@@ -79,11 +117,41 @@ plugin.methods.register_function(
         'foundation_taxonomy': FeatureData[Taxonomy],
             },
     parameters={
-        'graft_level': qiime2.plugin.Str,
+        'graft_level': graft_level,
                 },
     outputs=[
         ('ghost_tree', Phylogeny[Rooted]),
     ],
+    input_descriptions={
+        'otu_map': 'OtuMap. The output from the command "extensions-cluster". '
+                   'Each line in an OtuMap contains sequences that are a '
+                   'certain percent similar to one another.',
+        'extension_taxonomy': 'FeatureData[Taxonomy] The taxonomy for the '
+                              'extension sequences for a ghost tree.',
+        'extension_sequences': 'FeatureData[Sequence] The extension sequences '
+                               'for the ghost tree. The extensions are '
+                               'grafted onto a foundation tree.',
+        'foundation_tree': 'Phylogeny[Rooted | Unrooted] The foundation tree '
+                           'for your ghost tree. If you would like to use a '
+                           'foundation alignment, you can use the command '
+                           '"scaffold-hybrid-tree-foundation-alignment".',
+        'foundation_taxonomy': 'FeatureData[Taxonomy] The taxonomy for the '
+                               'foundation tree.',
+    },
+    parameter_descriptions={
+        'graft_level': 'The taxonomic rank you would like to group extension '
+                       'sequences by prior to having them grafted onto the '
+                       'foundation. Rank choices are: p: phylum, c: '
+                       'class, o: order, f: family, g: genus. Please note '
+                       'grafting at higher taxonomic ranks than genus is '
+                       'experimental, but may be useful for marker-gene '
+                       'regions which have less optimal taxonomic resolution.',
+    },
+    output_descriptions={
+        'ghost_tree': 'Phylogeny[Rooted]. This is your final ghost tree '
+                      'output as a .qza. Extracted it will be in Newick '
+                      'format.',
+    },
     name='scaffold-hybrid-tree-foundation-tree',
     description='Create a hybrid-gene phylogenetic tree using '
                 'a tree as a foundation.'
@@ -91,7 +159,7 @@ plugin.methods.register_function(
 
 # setup similarity threshold
 p = qiime2.plugin
-p = p.Float % p.Range(0.00, 1.00)
+p = p.Float % p.Range(0.00, 1.00, inclusive_end=True, inclusive_start=False)
 
 plugin.methods.register_function(
     function=extensions_cluster,
@@ -104,6 +172,28 @@ plugin.methods.register_function(
     outputs=[
         ('otu_map', OtuMap),
     ],
+    input_descriptions={
+        'extension_sequences': 'FeatureData[Sequence] The extension sequences '
+                               'for the ghost tree. The extensions are '
+                               'grafted onto a foundation tree, but should '
+                               'be first clustered to address the large '
+                               'amount of unclassified or unidentified '
+                               'sequences in taxonomic databases.',
+    },
+    parameter_descriptions={
+        'similarity_threshold': 'Value (float) greater than 0.00 and less '
+                                'than or equal to 1.00. This is the '
+                                'percent similarity you would like your '
+                                'sequences clustered at. E.g., 0.90 means '
+                                'that sequences in one group will be 90% '
+                                'similar to one another',
+    },
+    output_descriptions={
+         'otu_map': 'OtuMap. This returns an OtuMap where each line contains '
+                    'sequences that are a certain percent similar to one '
+                    'another as entered in the "--p-similarity-threshold" '
+                    'parameter. ',
+    },
     name='extensions-cluster',
     description='Groups sequences in .fasta file by similarity threshold'
 )
